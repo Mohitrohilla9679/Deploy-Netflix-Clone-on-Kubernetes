edb22916a0b23a1a8014c8cf4370a285543aca84
Mohit Rohilla <118180056+Mohitrohilla9679@users.noreply.github.com>
2024-02-20 00:05:10 +0530

    Update index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,7 +1,7 @@
 import { CustomGenre } from "src/types/Genre";
 
-export const API_ENDPOINT_URL = https://api.themoviedb.org/3;
-export const TMDB_V3_API_KEY = 8d794ed47b2a16ce64987d3f6c8b453f
+export const API_ENDPOINT_URL = "https://api.themoviedb.org/3";
+export const TMDB_V3_API_KEY = "8d794ed47b2a16ce64987d3f6c8b453f";
 
 export const MAIN_PATH = {
   root: "",
