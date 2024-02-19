15f7575525fe9747335ac5d868eb7b0deac09ffb
Mohit Rohilla <118180056+Mohitrohilla9679@users.noreply.github.com>
2024-02-20 00:40:45 +0530

    Update index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,7 +1,7 @@
 import { CustomGenre } from "src/types/Genre";
 
-export const API_ENDPOINT_URL = "netflix-unofficial.p.rapidapi.com";
-export const TMDB_V3_API_KEY = "2b78c29239mshf001e568a810df3p1b73b1jsnc222eb8b7693";
+export const API_ENDPOINT_URL = "https://api.themoviedb.org/3";
+export const TMDB_V3_API_KEY = "8d794ed47b2a16ce64987d3f6c8b453f";
 
 export const MAIN_PATH = {
   root: "",
